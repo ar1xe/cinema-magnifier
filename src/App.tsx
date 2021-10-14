559f454fa86f86fc6c780262a55a5416fa575afd
Alex Shikerun <chelseagrin52@gmail.com>
2021-10-14 23:52:20 +0300

    creating a clean project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
   return (
     <div className="App">
