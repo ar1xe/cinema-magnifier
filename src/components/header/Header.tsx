8fc9180cdf0ffc5724a24c31857a0cb07085d537
Alex Shikerun <chelseagrin52@gmail.com>
2021-10-19 21:49:14 +0300

    add npm package

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+
+const Header: FC = () => {
+  return (
+    <div>
+      <span>movies</span>
+      <span>serials</span>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/startPage/StartPage.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const StartPage: FC = () => {
+  return (
+    <div>
+      <h2>START PAGE</h2>
+    </div>
+  );
+};
+
+export default StartPage;
