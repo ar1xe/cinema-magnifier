b85919550eceeb03408b7f4b19e07c0420a2bb34
Alex Shikerun <chelseagrin52@gmail.com>
2021-10-20 00:14:45 +0300

    header svg and style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,18 +4,30 @@ import HeaderLogo from "../../assets/movies.svg";
 
 const HeaderWrapper = styled.div`
   width: 100%;
-  height: 70px;
+  height: 120px;
   display: flex;
-  background-color: #82d8ee;
+  background-color: #a6b6ac;
   justify-content: center;
+  // font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-family: "Roboto", sans-serif;
+  font-size: 22px;
+  font-weight: bold;
+  color: #003464;
 `;
 
 const HeaderContainer = styled.div`
   display: flex;
   width: 1050px;
-  height: auto;
-  justify-content: center;
+  height: 115px;
+  justify-content: flex-start;
   align-items: center;
+`;
+
+const NavBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 400px;
+  margin-left: 50px;
 `;
 
 const Header: FC = () => {
@@ -24,17 +36,16 @@ const Header: FC = () => {
       <HeaderWrapper>
         <HeaderContainer>
           <div>
-            <img src={HeaderLogo} alt="logo" />
+            <img src={HeaderLogo} alt="logo" width={120} height={100} />
           </div>
-          <div>
-            <span>movies</span>
-            <span>serials</span>
-          </div>
+          <NavBar>
+            <span>Movies</span>
+            <span>Serials</span>
+            <span>People</span>
+            <span>Yet</span>
+          </NavBar>
         </HeaderContainer>
       </HeaderWrapper>
-      {/* <div>
-        <img src={HeaderLogo} alt="logo" />
-      </div> */}
     </>
   );
 };
