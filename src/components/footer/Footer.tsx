8f4e49d3a5e8b55a5ec3112a3239884b064fbf84
Alex Shikerun <chelseagrin52@gmail.com>
2021-10-22 00:27:39 +0300

    fix styles footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,31 +1,10 @@
 import React, { FC } from "react";
-import styled from "styled-components";
 import Logo from "../../assets/logo.svg";
 import FB from "../../assets/facebook.svg";
 import Mail from "../../assets/mail.svg";
 import Reddit from "../../assets/reddit.svg";
 import Setting from "../../assets/setting.svg";
-
-const FooterWrapper = styled.div`
-  width: 100%;
-  height: 200px;
-  display: flex;
-  background-color: #002640;
-  justify-content: center;
-  align-items: center;
-  font-family: "Roboto", sans-serif;
-  font-size: 16px;
-  font-weight: bold;
-  color: #ffffff;
-`;
-
-const FooterContainer = styled.div`
-  display: flex;
-  width: 1050px;
-  height: 200px;
-  align-items: center;
-  justify-content: space-between;
-`;
+import { FooterWrapper, FooterContainer } from "./FooterStyledComponents";
 
 const Footer: FC = () => {
   return (
---
+++ src/components/footer/FooterStyledComponents.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const FooterWrapper = styled.div`
+  width: 100%;
+  height: 200px;
+  display: flex;
+  background-color: #002640;
+  justify-content: center;
+  align-items: center;
+  font-family: "Roboto", sans-serif;
+  font-size: 16px;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
+export const FooterContainer = styled.div`
+  display: flex;
+  width: 1050px;
+  height: 200px;
+  align-items: center;
+  justify-content: space-between;
+`;
