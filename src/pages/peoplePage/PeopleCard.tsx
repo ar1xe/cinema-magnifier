82a2af67683daf00a6aa2d7d6342d4d729074b9c
Alex Shikerun <chelseagrin52@gmail.com>
2021-12-13 22:59:20 +0300

    svg change color

--- src/pages/peoplePage/PeopleCard.tsx
+++ src/pages/peoplePage/PeopleCard.tsx
@@ -52,10 +52,31 @@ const Circle = styled.div.attrs((props: { isLiked: boolean }) => props)`
 //   fill: ${(props) => (props.isLiked ? `red` : `white`)};
 // `;
 
-const TitleBtn = styled.div`
+const TitleBtn = styled.div.attrs((props: { isLiked: boolean }) => props)`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   width: 130px;
+  label {
+    display: block;
+    /* pointer-events: none; */
+  }
+  #red {
+    display: none;
+    /* display: block; */
+  }
+  svg {
+    width: 25px;
+    display: block;
+  }
+  #heart {
+    fill: white;
+    stroke: #ffffff;
+    stroke-width: 30px;
+  }
+  #red:checked + svg #heart {
+    fill: #d64646;
+    stroke: #d64646;
+  }
 `;
 
 const PeopleCard: FC<CardPeopleProps> = ({
@@ -79,11 +100,31 @@ const PeopleCard: FC<CardPeopleProps> = ({
       <NameContainer>{name}</NameContainer>
       <div>
         <Button type="primary" onClick={onLikeClick}>
-          <TitleBtn>
-            <Circle isLiked={liked} />
+          <TitleBtn isLiked={liked}>
+            <label>
+              <input type="checkbox" id="red" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={25}
+                height={25}
+                viewBox="0 0 512 512"
+              >
+                <title>Heart</title>
+                <path
+                  id="heart"
+                  d="M352.92 80C288 80 256 144 256 144s-32-64-96.92-64c-52.76 0-94.54 44.14-95.08 96.81-1.1 109.33 86.73 187.08 183 252.42a16 16 0 0018 0c96.26-65.34 184.09-143.09 183-252.42-.54-52.67-42.32-96.81-95.08-96.81z"
+                  // fill="white"
+                  // stroke="white"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  // stroke-width="30"
+                />
+              </svg>
+            </label>
+            {/* <Circle isLiked={liked} /> */}
             {/* <img src={Like} alt="logo" width={25} height={25} isLiked={liked} /> */}
 
-            <LikeSVG id="LikeSVG" fill="white" stroke="white" />
+            {/* <LikeSVG id="LikeSVG" fill="white" stroke="white" /> */}
           </TitleBtn>
         </Button>
       </div>
