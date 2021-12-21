5671bcd9b6321126d1411267e2a12adefd4d93fe
Alex Shikerun <chelseagrin52@gmail.com>
2021-12-21 22:03:18 +0300

    test commit

--- src/services/FavoriteServices.ts
+++ src/services/FavoriteServices.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Peoples } from "../pages/peoplePage/PeoplePage";
-import { MovieCardProps, Movies } from "../pages/startPage/StartPage";
+import { Movies } from "../pages/startPage/StartPage";
 
 const BASE_URL = "http://localhost:3333/favorite/";
 
